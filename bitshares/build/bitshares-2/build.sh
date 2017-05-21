510775332d7a6f80323729f85951d0fba4fd170e
crazybit <273593465@qq.com>
2017-05-21 12:39:44 +0800

    update composer files

--- bitshares/build/bitshares-2/build.sh
+++ bitshares/build/bitshares-2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export TAG=2.0.160829
-cat Dockerfile | envsubst | docker build --no-cache -t bitshares/bitshares-2:${TAG} -
+cat Dockerfile | envsubst | docker build --no-cache -t crazybit/bitshares-2:${TAG} -
