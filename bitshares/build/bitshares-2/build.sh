ebfe7cc987405524e68671d3430a919998cc33f9
crazybit <273593465@qq.com>
2017-05-21 13:53:40 +0800

    only clone on master branch

--- bitshares/build/bitshares-2/build.sh
+++ bitshares/build/bitshares-2/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-export TAG=2.15.306
-cat Dockerfile | envsubst | docker build --no-cache -t crazybit/bitshares-2:${TAG} -
+cat Dockerfile | envsubst | docker build --no-cache -t crazybit/bitshares-2:lastest -
